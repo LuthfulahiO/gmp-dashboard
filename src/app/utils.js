b979fac954b686ab90ebec03f249752e2cf95ff4
Luthfulahi <luteeph@gmail.com>
2019-12-04 12:38:13 +0100

    fixed pre hook

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,56 +1,52 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const isProd = process.env.NODE_ENV === "production";
+export const isProd = process.env.NODE_ENV === 'production'
 
 const request = axios.create({
-  baseURL:  "http://portal.gmp.dervac.com/api/v1"
-});
+  baseURL: 'https://portal.gmp.dervac.com/api/v1',
+})
 
-export const callApi = (url, data = {}, methodName = "get") => {
-  const method = methodName.toLowerCase();
+export const callApi = (url, data = {}, methodName = 'get') => {
+  const method = methodName.toLowerCase()
   return new Promise((resolve, reject) => {
     const requestConfig = {
       headers: {
-        Authorization: "Bearer " + LocalStore.getToken()
+        Authorization: 'Bearer ' + LocalStore.getToken(),
       },
       method,
-      url
-    };
-    if (method === "get") {
-      requestConfig.params = data;
+      url,
+    }
+    if (method === 'get') {
+      requestConfig.params = data
     } else {
-      requestConfig.data = data;
+      requestConfig.data = data
     }
     request(requestConfig)
       .then(res => resolve(res.data))
       .catch(err => {
         if (err.response) {
           // Response from server w/ error
-          return reject(err.response.data);
+          return reject(err.response.data)
         }
-        return reject({
-          title: "❌ Network Error",
-          message: "Please check your internet connection and try again."
-        });
-      });
-  });
-};
+      })
+  })
+}
 
 export const LocalStore = {
-  saveToken(token, key = "gmp_auth_token") {
-    return localStorage.setItem(key, token);
+  saveToken (token, key = 'gmp_auth_token') {
+    return localStorage.setItem(key, token)
   },
-  saveUser(data, key = "gmp_user"){
+  saveUser (data, key = 'gmp_user') {
     return localStorage.setItem(key, JSON.stringify(data))
   },
-  logUserOut(){
-    localStorage.removeItem("gmp_auth_token")
-    return localStorage.removeItem("gmp_user")
+  logUserOut () {
+    localStorage.removeItem('gmp_auth_token')
+    return localStorage.removeItem('gmp_user')
   },
-  getUser(key = "gmp_user"){
+  getUser (key = 'gmp_user') {
     return JSON.parse(localStorage.getItem(key))
   },
-  getToken(key = "gmp_auth_token") {
-    return localStorage.getItem(key);
-  }
-};
+  getToken (key = 'gmp_auth_token') {
+    return localStorage.getItem(key)
+  },
+}
--- src/components/jobs/apply.vue
+++ src/components/jobs/apply.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="file-upload">
       <h1>Apply for job</h1>
+      <va-notification v-if="errorMessageFromServer" color="danger" closeable>
+          <va-badge color="danger">
+            Error
+          </va-badge>
+          {{ errorMessageFromServer }}
+        </va-notification>
+        <va-notification v-if="successMessage" color="success" closeable>
+          <va-badge color="success">
+            Success
+          </va-badge>
+          {{ successMessage }}
+        </va-notification>
     <div class="row">
       <div class="flex xs12">
         <va-card title="CV">
@@ -37,40 +49,46 @@
 </template>
 
 <script>
-import {callApi} from '@/app/utils'
+import { callApi } from '@/app/utils'
 export default {
   name: 'apply',
   data () {
     return {
-      isLoading:false,
-      cv: "",
-      coverletter: "",
-      description: "",
-      jobuuid: ""
+      errorMessageFromServer: null,
+      successMessage: null,
+      isLoading: false,
+      cv: '',
+      coverletter: '',
+      description: '',
+      jobuuid: '',
     }
   },
   created () {
-    if(!this.$route.params.uuid) {
-        this.$router.push('/joblist')
-    }else {
-        this.jobuuid = this.$route.params.uuid
+    if (!this.$route.params.uuid) {
+      this.$router.push('/joblist')
+    } else {
+      this.jobuuid = this.$route.params.uuid
     }
   },
   methods: {
     submitApplication () {
-      let self = this;
+      let self = this
       this.isLoading = true
       let formData = new FormData()
-      formData.append("cv", self.cv[0])
-      formData.append("cover", self.coverletter[0])
-      formData.append("description", self.description)
+      formData.append('cv', self.cv[0])
+      formData.append('cover', self.coverletter[0])
+      formData.append('description', self.description)
 
-      callApi(`/jobs/apply/${self.jobuuid}`, formData, "post")
+      callApi(`/jobs/apply/${self.jobuuid}`, formData, 'post')
         .then(res => {
-            console.log(res);
+          this.errorMessageFromServer = null
+          this.successMessage = res.message
+          this.cv = ''
+          this.coverletter = ''
         })
         .catch(err => {
-            console.log(err);
+          this.successMessage = null
+          this.errorMessageFromServer = err.message
         })
     },
   },
